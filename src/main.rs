8fb546509570c525a8b073fed02e25f631a00c18
jam1garner <jam1.mcleod@hotmail.com>
2019-02-21 13:27:51 -0500

    Add handlebars template handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+
+use rocket_contrib::templates::Template;
+
+#[get("/")]
+fn index() -> Template {
+    Template::render("index", 
+
+json![{
+    "posts" : [
+        {
+            "title": "test",
+            "date": "01-01-2018"
+        },
+        {
+            "title": "test 2",
+            "date": "02-03-2019"
+        }
+    ],
+    "test" : "3"
+}]
+)}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .attach(Template::fairing())
+        .mount("/", routes![index]).launch();
 }
