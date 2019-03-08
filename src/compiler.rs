c17ad6faa937866841d4eef9e68ca1f6c901a123
jam1garner <jam1.mcleod@hotmail.com>
2019-03-08 00:03:33 -0500

    Add msclang compiler explorer backend piping

---
+++ src/compiler.rs
@@ -0,0 +1,27 @@
+use std::fs;
+use std::process::{Command, Stdio, ChildStdout};
+
+use rocket::data::Data;
+use rocket::response::{content, Stream};
+use rocket_contrib::json::JsonValue;
+
+pub fn compile(file: Data) -> Option<content::Json<Stream<ChildStdout>>> {
+    let process = Command::new("python3")
+                    .arg("msclang_explorer/msclang.py")
+                    .stdin(Stdio::piped())
+                    .stdout(Stdio::piped())
+                    .spawn()
+                    .ok()?;
+    
+    file.stream_to(&mut process.stdin?).ok()?;
+
+    Some(content::Json(Stream::from(process.stdout?)))
+}
+
+pub fn get_default() -> Option<JsonValue> {
+    Some(
+        json![{
+            "program" : fs::read_to_string("templates/default.c").ok()?
+        }]
+    )
+}
--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,15 @@ extern crate serde_json;
 mod blog_data;
 mod project_data;
 mod feed;
+mod compiler;
 
-use rocket::response::NamedFile;
-use rocket::response::Redirect;
+use rocket::response::{content, Redirect, Stream, NamedFile};
+use rocket::data::Data;
 use rocket_contrib::templates::Template;
 use rocket_contrib::json::JsonValue;
 
 use std::path::{Path, PathBuf};
+use std::process::ChildStdout;
 
 #[get("/")]
 fn index() -> Template {
@@ -56,10 +58,31 @@ fn blog_post_raw(file: String) -> Option<NamedFile> {
     NamedFile::open(Path::new("posts/").join(file)).ok()
 }
 
+#[post("/compile", format="text/plain", data="<file>")]
+fn compile(file: Data) -> Option<content::Json<Stream<ChildStdout>>> {
+    compiler::compile(file)
+} 
+
+#[get("/compiler")]
+fn compiler_explorer() -> Option<Template> {
+    Some(
+        Template::render(
+            "compiler_explorer",
+            // Template data, passes in the default program
+            compiler::get_default()
+                .unwrap_or(
+                    json![{
+                        "program" : "void main(){\n}"
+                    }]
+                )
+        )
+    )
+} 
+
 fn main() {
      rocket::ignite()
         .attach(Template::fairing())
-        .mount("/", routes![index, posts, blog, projects])
+        .mount("/", routes![index, posts, blog, projects, compile, compiler_explorer])
         .mount("/css/", routes![css])
         .mount("/img/", routes![img])
         .mount("/blog/", routes![blog_post, blog_post_raw])
